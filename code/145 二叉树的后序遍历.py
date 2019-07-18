352e3798c1fa03c8f1bc97a58a3bc06e4592e651
lmz <lmzzzzz1@163.com>
2019-07-18 18:08:39 +0800

    19/7/18 by lmz

--- code/145 二叉树的后序遍历.py
+++ code/145 二叉树的后序遍历.py
@@ -12,12 +12,14 @@
 输出: [3,2,1]
 '''
 
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
+
 
 class Solution:
     def postorderTraversal(self, root):
@@ -26,25 +28,25 @@ class Solution:
         ans = []
         stack = []
         last = None
-        while root or stack!=[]:
+        while root or stack != []:
             while root:
                 stack.append(root)
                 root = root.left
             current = stack[-1]
             if not current.right or current.right == last:
-                    ans.append(current.val)
-                    stack.pop()
-                    last = current
-                    root = None
-            else:    
-                    root = current.right        
-        return ans        
+                ans.append(current.val)
+                stack.pop()
+                last = current
+                root = None
+            else:
+                root = current.right
+        return ans
 
     def postorderTraversal_reverse(self, root):
-    # 前序遍历是根，左，右，那么按照根，右，左的方法输出，很简单反转一下就能得到
+        # 前序遍历是根，左，右，那么按照根，右，左的方法输出，很简单反转一下就能得到
         ans = []
         stack = []
-        while root or stack!=[]:
+        while root or stack != []:
             if root:
                 ans.append(root.val)
                 stack.append(root)
@@ -53,5 +55,4 @@ class Solution:
                 current = stack.pop()
                 root = current.left
 
-
-        return ans[::-1]                  
+        return ans[::-1]
